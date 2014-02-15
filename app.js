784793b5b20a862e69c67d3eae0b0356f92934b3
Siddharth Murching <smurching@gmail.com>
2014-02-15 11:39:39 -0800

    typo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var expressValidator = require('express-validator');
 var SummaryTool = require('node-summary');
 var AWS = require('aws-sdk')
 var nodecr = require('nodecr')
-var config = AWS.Config({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: 'us-west-2'})
+var config = new AWS.Config({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: 'us-west-2'})
 var s3 = new AWS.S3({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: 'us-west-2'}); 
 
 
